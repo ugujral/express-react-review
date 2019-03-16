d61aaf0040b64749f61b2ae9d0730486f06f1120
Yongsoo Lee <yongsoo07@gmail.com>
2019-03-16 15:10:00 -0700

    exercise 1 complete

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-// Express here
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+
+const server = express();
+
+const port = 3000;
+
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+
+server.listen(port, () => console.log(`Connected to port ${port}!`));
